aa586688eb53e9beb7170978f0dfd4bb385a3f8b
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-01-22 15:14:45 +0500

    updated the frontend code for development testing

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { redirect } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
